14d57bbdf0640421dfb5ff04e88a451241c2c551
panorame3magali <Pacha.Huo@gmail.com>
2018-06-21 13:05:59 -0300

    Mi app Django Girls, primer commit

---
+++ practicas/miprimerprint.py
@@ -0,0 +1,18 @@
+def printeando(name):
+
+    print("Hola mundo biteros")
+    if 3 > 2:
+        print("programando con Python")
+    if 3 < 2:
+        print("entrando a lo verdadero o tal vez a lo falso")
+    else:
+        print("el else es verdadero y falso")
+name  = "Magali"
+if [name == "Mona"]:
+    print("no sale por aca")
+elif name == "Magali":
+    print("llegamos al elif")
+else:
+    print("escribe tu código")
+
+printeando("Mona")
